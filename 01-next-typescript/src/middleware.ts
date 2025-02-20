55bc6b3fc293d299419cdd0c63c37e864304c883
touya875 <ishiitouya@gmail.com>
2025-02-20 22:56:36 +0700

    FE - complete register and send verification code with email

--- 01-next-typescript/src/middleware.ts
+++ 01-next-typescript/src/middleware.ts
@@ -2,7 +2,8 @@ export { auth as middleware } from "@/auth";
 
 export const config = {
   matcher: [
-    "/((?!auth).*)(.+)|/verify",
-    // "/((?!api|_next/static|_next/image|favicon.ico|).*)|/",
+    // '/((?!auth).*)(.+)|/verify',
+    // "/((?!api|_next/static|_next/image|favicon.ico|/|/auth).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|auth|verify|$).*)",
   ],
 };
